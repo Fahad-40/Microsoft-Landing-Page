fc8fc7d9a9b5f1be9cab57a1f01aadcfc0314430
Fahad <fahad05munir@gmail.com>
2026-06-20 15:17:54 +0500

    code updated

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,17 @@
     const hamburgerBtn = document.getElementById('hamburgerBtn');
     const mobileMenu = document.getElementById('mobileMenu');
 
-    hamburgerBtn.addEventListener("click" , () =>{
+    hamburgerBtn.addEventListener("click" , (e) =>{
 mobileMenu.classList.toggle("translate-class");
 e.stopPropagation();
 console.log("button clicked!");
     });
 
 
-  document.addEventListener("click" , () =>{
+  document.addEventListener("click" , (e) =>{
 
+if (!mobileMenu.contains(e.target)) {
+mobileMenu.classList.add("translate-class");    
+}
 
-    
   })  
